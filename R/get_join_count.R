ad44e3169fe38855dc600993f8f0e39e1d35c154
Rtoeb <65813696+toebR@users.noreply.github.com>
2021-01-25 11:07:10 +0100

    updated with suppress messages

---
+++ R/get_join_count.R
@@ -0,0 +1,24 @@
+#' Count Join ID in a dataset
+#'
+#' @param data_x data
+#' @param ID_x Join ID in dataset x
+#' @return df with how many times an ID appears in a dataset
+#' @export
+#' @examples
+#' ##get_join_count(df.x, df.x$ID)
+
+
+
+# function that generates a join count for each ID
+
+get_join_count <- function(data_x, ID_x) {
+  suppressWarnings(
+  data_x %>%
+    dplyr::group_by({{ID_x}}) %>%
+    dplyr::summarise(n = dplyr::n()) %>%
+    dplyr::rename(join_count = n) -> join_c
+)
+  return(join_c)
+
+}
+
