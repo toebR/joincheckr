41501bb3ca49724a39ac53c2ae2167e5d0314add
delabj <j.l.delabruere@gmail.com>
2021-01-25 08:10:11 -0600

    Fix notes due to tidyEval
    This adds a dependancy on rlang but stops the notes about no visiable bindings

--- R/get_join_count.R
+++ R/get_join_count.R
@@ -4,6 +4,7 @@
 #' @param ID_x Join ID in dataset x
 #' @return df with how many times an ID appears in a dataset
 #' @export
+#' @importFrom rlang .data
 #' @examples
 #' ##get_join_count(df.x, df.x$ID)
 
@@ -12,11 +13,12 @@
 # function that generates a join count for each ID
 
 get_join_count <- function(data_x, ID_x) {
+
   suppressWarnings(
   data_x %>%
     dplyr::group_by({{ID_x}}) %>%
     dplyr::summarise(n = dplyr::n()) %>%
-    dplyr::rename(join_count = n) -> join_c
+    dplyr::rename(join_count = .data$n) -> join_c
 )
   return(join_c)
 
